ad117f45fda72411b92eb257398b4b2e2f103144
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-06-01 21:15:08 -0300

    readd generateStaticParams

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -34,6 +34,13 @@ export async function generateMetadata({
   }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => ({ slug: product.slug }))
+}
+
 export default async function ProductPage({ params }: ProductPageProps) {
   const product = await getProduct(params.slug)
 
